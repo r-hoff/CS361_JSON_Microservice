6a2c9a9aaad15d8983a4a661df7656936d098979
Ryan Hoff <ryanchoff@gmail.com>
2022-05-11 16:18:01 -0700

    clean up

--- server.py
+++ server.py
@@ -1,30 +1,33 @@
+import sys
 import zmq
 from validateJson import validateJson
 from expenseReport import createExpenseReport
 
-# the port that the server will run on
-PORT = 6000
+if __name__ == '__main__':
 
-# set up a socket connection on localhost:PORT
-context = zmq.Context().instance()
-socket = context.socket(zmq.REP)
-socket.bind(f"tcp://127.0.0.1:{PORT}")
+    if len(sys.argv) > 1:
+        # the port that the server will run on
+        PORT = sys.argv[1]
 
-while True:
-    #  Wait for request from client
-    request = socket.recv_string()
-    print('Request from client: ' + request)
+    # set up a socket connection on localhost:PORT
+    context = zmq.Context().instance()
+    socket = context.socket(zmq.REP)
+    socket.bind(f"tcp://127.0.0.1:{PORT}")
 
-    # ensure that the request is a valid JSON object and matches expected format
-    jsonObj = validateJson(request)
-    if not jsonObj:
-        errorMsg = "Error: Request is not in valid JSON format"
-        socket.send_string(errorMsg)
-        print('Response to client: ' + errorMsg)
-    else:
-        # if valid, create an expense report
-        filepath = createExpenseReport(jsonObj)
+    while True:
+        #  Wait for request from client
+        request = socket.recv_string()
 
-        # send filepath back to client
-        socket.send_string(filepath)
-        print('Response to client: ' + filepath)
+        # ensure that the request is a valid JSON object and matches expected format
+        jsonObj = validateJson(request)
+        if not jsonObj:
+            errorMsg = "Error: Request is not in valid JSON format"
+            socket.send_string(errorMsg)
+
+        else:
+            # if valid, create an expense report
+            filepath = createExpenseReport(jsonObj)
+
+            # send filepath back to client
+            socket.send_string(filepath)
+
